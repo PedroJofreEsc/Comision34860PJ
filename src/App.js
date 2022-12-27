c2ca4ed35fd1436d4929e25266760a8b1cd4b8be
pedroJ <pedro.jofre.escarate@gmail.com>
2022-12-26 21:59:20 -0300

    cartwidget itemlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/navbar/Navbar';
+import Cartwidget from './component/Cartwidget/cartwidget';
+import ItemListContainer from './component/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Cartwidget />
         <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.asd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ItemListContainer greeting={"hola"} />
+
       </header>
     </div>
   );
---
+++ src/component/Cartwidget/Cartwidget.js
@@ -0,0 +1,10 @@
+const Cartwidget = () => {
+    return (
+        <div>
+            <img src='./images/shopping-cart-3.svg' alt="" />
+
+        </div>
+    )
+}
+
+export default Cartwidget
